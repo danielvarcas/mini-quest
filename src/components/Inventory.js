316d6961f292620b78751dc01497d4d4f051466b
Daniel Varcas <daniel.varcas@gmail.com>
2018-12-17 12:36:21 +0000

    first commit

---
+++ src/components/Inventory.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Inventory = () => {
+  return (
+    <div id="inventory">
+      <table>
+        <tr>
+          <th>Weapon:</th>
+          <td>None</td>
+        </tr>
+        <tr>
+          <th>Armour:</th>
+          <td>None</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default Inventory;
